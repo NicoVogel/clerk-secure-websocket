aec012ba56a5343a610425edecf4aaa33aa14f94
Nico Vogel <nico-vogel97@outlook.de>
2025-02-19 18:48:27 +0100

    add auth for websocket connection

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,4 +1,7 @@
+import { createClerkClient } from "@clerk/backend";
+import { SignedInAuthObject } from "@clerk/backend/internal";
 import http from "node:http";
+import internal from "node:stream";
 import { WebSocketServer } from "ws";
 
 const server = http.createServer((req, res) => {
@@ -29,18 +32,89 @@ wss.on("connection", (ws, req) => {
   console.log("connection");
   let index = 0;
   ws.on("message", (message) => {
-    console.log("message", message);
-    ws.send(`pong ${index++}`);
+    const auth: SignedInAuthObject = (ws as any).auth;
+    console.log("message", message.toString(), auth.userId);
+    ws.send(`pong ${index++} ${auth.userId}`);
   });
 });
 
-server.on("upgrade", (req, socket, header) => {
+server.on("upgrade", async (req, socket, header) => {
+  const auth = await authGuard(req, socket);
+  if (auth === undefined) {
+    return;
+  }
+
   wss.handleUpgrade(req, socket, header, (ws) => {
+    // the effort of globally extending the WebSocket type with the `auth` property is not worth it
+    (ws as any).auth = auth;
     wss.emit("connection", ws, req);
-  })
+  });
 });
 
 const port = 3000;
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
+
+const clerkClient = createClerkClient({
+  secretKey: process.env.CLERK_SECRET_KEY,
+  publishableKey: process.env.VITE_CLERK_PUBLISHABLE_KEY,
+});
+
+function authGuard(req: http.IncomingMessage, socket: internal.Duplex) {
+  return clerkClient
+    .authenticateRequest(convertIncomingMessageToRequest(req))
+    .catch((err) => {
+      console.error(
+        `Something went wrong while authenticating the user: ${String(err)}`
+      );
+      respondUnauthorized(socket);
+    })
+    .then((client) => {
+      if (client === undefined) {
+        return;
+      }
+      if (client.isSignedIn === false) {
+        return respondUnauthorized(socket);
+      }
+      return client.toAuth();
+    })
+    .catch((err) => {
+      console.error(
+        `Something went wrong while upgrading the connection to WebSocket: ${String(
+          err
+        )}`
+      );
+      return responseInternalServerError(socket);
+    });
+}
+
+function convertIncomingMessageToRequest(req: http.IncomingMessage) {
+  const { method } = req;
+  const origin = `http://${req.headers.host ?? "localhost"}`;
+  const fullUrl = new URL(req.url!, origin);
+
+  const headers = new Headers();
+  for (const [key, value] of Object.entries(req.headers)) {
+    if (value === undefined || Array.isArray(value)) {
+      continue;
+    }
+    headers.set(key, value);
+  }
+
+  return new Request(fullUrl, {
+    method,
+    headers,
+  });
+}
+
+
+function respondUnauthorized(socket: internal.Duplex) {
+  socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
+  socket.destroy();
+}
+
+function responseInternalServerError(socket: internal.Duplex) {
+  socket.write("HTTP/1.1 500 Internal Server Error\r\n\r\n");
+  socket.destroy();
+}
